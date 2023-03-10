08e7c687292898aa79c2387be94f51b98d0c6c31
vekaste <vekaste@stud.ntnu.no>
2023-03-10 14:02:06 +0100

    feat(GroupTest): added tests for the methods in the group class.

---
+++ src/test/java/no/ntnu/idatt1002/demo/data/GroupTest.java
@@ -0,0 +1,48 @@
+package no.ntnu.idatt1002.demo.data;
+
+import no.ntnu.idatt1002.demo.data.person.Member;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupTest {
+
+  @Test
+  void addMemberTest() {
+    Member newMember = new Member(123123,new Date(12-11-1212));
+    Group group = new Group(123131,"NewGroup");
+    group.addMember(newMember);
+    assertFalse(group.getGroup().isEmpty());
+  }
+
+  @Test
+  void removeMemberTest() {
+    Member newMember = new Member(123123,new Date(12-11-1212));
+    Group group = new Group(123131,"NewGroup");
+    group.addMember(newMember);
+    assertFalse(group.getGroup().isEmpty());
+    group.removeMember(newMember);
+    assertTrue(group.getGroup().isEmpty());
+  }
+
+  @Test
+  void getGroupIdTest() {
+    Group group = new Group(123131,"NewGroup");
+    assertEquals(123131, group.getGroupId());
+  }
+
+  @Test
+  void getGroupNameTest() {
+    Group group = new Group(123131,"NewGroup");
+    assertEquals("NewGroup", group.getGroupName());
+  }
+
+  @Test
+  void setGroupNameTest() {
+    Group group = new Group(123131,"NewGroup");
+    group.setGroupName("NewGroup2");
+    assertEquals("NewGroup2", group.getGroupName());
+  }
+}
