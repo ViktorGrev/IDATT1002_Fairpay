edaaabd0ba83110abb31091568e0308236acdf22
henridb <henridb@stud.ntnu.no>
2023-03-10 13:11:40 +0100

    Add Member and Person

--- src/main/java/no/ntnu/idatt1002/demo/data/person/Member.java
+++ src/main/java/no/ntnu/idatt1002/demo/data/person/Member.java
@@ -1,4 +1,22 @@
 package no.ntnu.idatt1002.demo.data.person;
 
-public class Member {
+import java.util.Date;
+
+public final class Member {
+
+    private final long personId;
+    private final Date joinDate;
+
+    public Member(long personId, Date joinDate) {
+        this.personId = personId;
+        this.joinDate = joinDate;
+    }
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
 }
