bcb1aa3e2b71591206b8f2c6574a9fef1ac2b94f
vekaste <vekaste@stud.ntnu.no>
2023-03-10 13:14:50 +0100

    feat(Group): added functionality to methods add and remove, aswell as adding getters and setters.

--- src/main/java/no/ntnu/idatt1002/demo/data/Group.java
+++ src/main/java/no/ntnu/idatt1002/demo/data/Group.java
@@ -1,4 +1,57 @@
 package no.ntnu.idatt1002.demo.data;
 
+import no.ntnu.idatt1002.demo.data.person.Member;
+
+import java.util.HashMap;
+
 public class Group {
+  HashMap<Member.getId(),Member> gruop = new HashMap<Member.getId(),Member>();
+  private int groupId;
+  private String groupName;
+
+
+  public Group(int groupId, String groupName) {
+    this.groupId = groupId;
+    this.groupName = groupName;
+  }
+
+  public boolean addMember(Member member) {
+    if (group.books.entrySet().stream()
+            .filter(e -> (member.getId).equals(e.getValue()))
+            .map(group.Entry::getKey)
+            .findFirst()){
+      return false;
+    } else {
+      group.put(member.getId(), member);
+      return true;
+    }
+  }
+
+  public boolean removeMember(Member member) {
+    if(group.books.entrySet().stream()
+            .filter(e -> (member.getId).equals(e.getValue()))
+            .map(group.Entry::getKey)
+            .findFirst()){
+      group.remove(member.getId());
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public int getGroupId() {
+    return groupId;
+  }
+
+  public void setGroupId(int groupId) {
+    this.groupId = groupId;
+  }
+
+  public String getGroupName() {
+    return groupName;
+  }
+
+  public void setGroupName(String groupName) {
+    this.groupName = groupName;
+  }
 }
