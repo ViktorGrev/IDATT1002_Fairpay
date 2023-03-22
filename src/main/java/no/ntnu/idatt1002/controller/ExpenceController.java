dc1bf2138ca87972579697eb890675d4e9997c56
vikto <viktog2210@gmail.com>
2023-03-22 12:12:22 +0100

    Changing name to match content

--- src/main/java/no/ntnu/idatt1002/controller/ExpenceController.java
+++ src/main/java/no/ntnu/idatt1002/controller/ExpenceController.java
@@ -1,15 +1,11 @@
 package no.ntnu.idatt1002.controller;
 
-import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import no.ntnu.idatt1002.SceneSwitcher;
 
-import javax.swing.text.html.ImageView;
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
-public class SettlementController {
+public class ExpenceController {
 
   public void testButtonClick(MouseEvent mouseEvent) throws IOException {
     SceneSwitcher.setView("test");
