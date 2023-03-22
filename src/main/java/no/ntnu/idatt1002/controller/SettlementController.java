9392a175bb60a54fc90c9abec81a80ec9a0bb3e0
vekaste <vekaste@stud.ntnu.no>
2023-03-22 11:20:12 +0100

    feat(settlementController): added controller functions to switch scene.

--- src/main/java/no/ntnu/idatt1002/controller/SettlementController.java
+++ src/main/java/no/ntnu/idatt1002/controller/SettlementController.java
@@ -1,4 +1,21 @@
 package no.ntnu.idatt1002.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import no.ntnu.idatt1002.SceneSwitcher;
+
+import javax.swing.text.html.ImageView;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class SettlementController {
+
+  public void testButtonClick(MouseEvent mouseEvent) throws IOException {
+    SceneSwitcher.setView("test");
+  }
+
+  public void settlementButtonClicked(MouseEvent mouseEvent) throws IOException {
+    SceneSwitcher.setView("test");
+  }
 }
