2f8c7273fb3488b31c0e3cd2455cd5e96b952b17
vekaste <vekaste@stud.ntnu.no>
2023-03-23 14:05:36 +0100

    feat(newSettlement.fxml/newSettlementController): added connection newSettlement and addExpense.

---
+++ src/main/java/no/ntnu/idatt1002/controller/newSettlementController.java
@@ -0,0 +1,15 @@
+package no.ntnu.idatt1002.controller;
+
+import javafx.event.ActionEvent;
+import no.ntnu.idatt1002.SceneSwitcher;
+
+import java.io.IOException;
+
+public class newSettlementController {
+  public void addExpenseClick(ActionEvent actionEvent) throws IOException {
+    SceneSwitcher.setView("addExpense");
+  }
+
+  public void removeExpenseClick(ActionEvent actionEvent) {
+  }
+}
