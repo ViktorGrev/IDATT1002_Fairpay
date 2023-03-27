baf236139ef6a7dd16c7f3fb8c1149c2f486bc9a
vekaste <vekaste@stud.ntnu.no>
2023-03-27 16:01:13 +0200

    feat(AddExpenseController): removed all the duplicate code in AddExpenseController.

--- src/main/java/no/ntnu/idatt1002/controller/AddExpenseController.java
+++ src/main/java/no/ntnu/idatt1002/controller/AddExpenseController.java
@@ -6,36 +6,8 @@ import no.ntnu.idatt1002.Scenes.SceneSwitcher;
 
 import java.io.IOException;
 
-public class AddExpenseController {
+public class AddExpenseController extends Controller{
   public void addExpenseClick(ActionEvent actionEvent) throws IOException {
     SceneSwitcher.setView("expense");
-  }
-
-  public void expenseClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("expense");
-  }
-
-  public void dormGroupClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("dormGroup");
-  }
-
-  public void budgetClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("budget");
-  }
-
-  public void settlementClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("settlement");
-  }
-
-  public void statusClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("status");
-  }
-
-  public void homeClick(MouseEvent mouseEvent) {
-    SceneSwitcher.setView("homepage");
-  }
-
-  public void incomeClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("income");
   }
 }
