eb9eced17c6be5fe23dcf19992109c4f9ffd3b71
vekaste <vekaste@stud.ntnu.no>
2023-03-27 16:07:23 +0200

    feat(profileController): removed all the duplicate code and added functionality to profile button.

--- src/main/java/no/ntnu/idatt1002/controller/ProfileController.java
+++ src/main/java/no/ntnu/idatt1002/controller/ProfileController.java
@@ -5,31 +5,5 @@ import no.ntnu.idatt1002.Scenes.SceneSwitcher;
 
 import java.io.IOException;
 
-public class ProfileController {
-    public void homeClick(ActionEvent actionEvent) throws IOException {
-        SceneSwitcher.setView("homePage");
-    }
-    public void expenseClick(ActionEvent actionEvent) throws IOException {
-        SceneSwitcher.setView("expense");
-    }
-
-    public void statusClick(ActionEvent actionEvent) throws IOException{
-        SceneSwitcher.setView("status");
-    }
-
-    public void settlementClick(ActionEvent actionEvent) throws IOException{
-        SceneSwitcher.setView("settlement");
-    }
-
-    public void incomeClick(ActionEvent actionEvent) throws IOException{
-        SceneSwitcher.setView("income");
-    }
-
-    public void budgetClick(ActionEvent actionEvent) {
-        SceneSwitcher.setView("budget");
-    }
-
-    public void dormGroupClick(ActionEvent actionEvent) {
-        SceneSwitcher.setView("dormGroup");
-    }
+public class ProfileController extends Controller{
 }
