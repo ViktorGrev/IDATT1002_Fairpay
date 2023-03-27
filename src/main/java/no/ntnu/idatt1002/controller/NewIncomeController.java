ba397f64996e4c863e37f344d6405a533c7e0763
vekaste <vekaste@stud.ntnu.no>
2023-03-27 16:06:52 +0200

    feat(NewIncomeController): removed all the duplicate code and added functionality to profile button.

--- src/main/java/no/ntnu/idatt1002/controller/NewIncomeController.java
+++ src/main/java/no/ntnu/idatt1002/controller/NewIncomeController.java
@@ -4,38 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.input.MouseEvent;
 import no.ntnu.idatt1002.Scenes.SceneSwitcher;
 
-public class NewIncomeController {
-  public void homeClick(MouseEvent mouseEvent) {
-    SceneSwitcher.setView("homepage");
-  }
-
-  public void profileClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("profile");
-  }
-
-  public void dormGroupClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("dormGroup");
-  }
-
-  public void groupExpenseClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("expense");
-  }
-
-  public void incomeClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("income");
-  }
-
-  public void budgetClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("budget");
-  }
-
-  public void settlementClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("dormGroup");
-  }
-
-  public void statusClick(ActionEvent actionEvent) {
-    SceneSwitcher.setView("status");
-  }
+public class NewIncomeController extends Controller{
 
   public void newIncomeConfirmClick(ActionEvent actionEvent) {
     SceneSwitcher.setView("income");
