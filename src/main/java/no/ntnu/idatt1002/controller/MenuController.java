731f9fd4aac87da726a626a1eefa834f8e3e641a
vikto <viktog2210@gmail.com>
2023-04-13 11:12:19 +0200

    feat(MenuController): Adding alert conformation box when logout

--- src/main/java/no/ntnu/idatt1002/controller/MenuController.java
+++ src/main/java/no/ntnu/idatt1002/controller/MenuController.java
@@ -1,6 +1,8 @@
 package no.ntnu.idatt1002.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import no.ntnu.idatt1002.data.Group;
 import no.ntnu.idatt1002.data.User;
 import no.ntnu.idatt1002.scene.SceneSwitcher;
@@ -73,8 +75,15 @@ abstract class MenuController extends Controller {
      */
     @FXML
     private void logoutButtonClick() {
-        Group.setCurrent(null);
-        User.setCurrent(null);
-        SceneSwitcher.setView("login");
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about to logout!");
+        alert.setContentText("Do you wish to logout?");
+
+        if(alert.showAndWait().get() == ButtonType.OK){
+            Group.setCurrent(null);
+            User.setCurrent(null);
+            SceneSwitcher.setView("login");
+        }
     }
 }
