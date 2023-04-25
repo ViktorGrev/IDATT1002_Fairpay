e6ce71852d73a514a66a2ef957adf092896ee7aa
henridb <henridb@stud.ntnu.no>
2023-04-26 00:11:03 +0200

    Remove legacy code

--- src/main/java/no/ntnu/idatt1002/controller/SettlementEditController.java
+++ src/main/java/no/ntnu/idatt1002/controller/SettlementEditController.java
@@ -13,16 +13,17 @@ import javafx.scene.paint.Paint;
 import no.ntnu.idatt1002.data.Group;
 import no.ntnu.idatt1002.data.User;
 import no.ntnu.idatt1002.data.economy.Expense;
-import no.ntnu.idatt1002.data.economy.ExpenseType;
 import no.ntnu.idatt1002.data.economy.Settlement;
 import no.ntnu.idatt1002.scene.Page;
 import no.ntnu.idatt1002.util.DateUtil;
 import no.ntnu.idatt1002.util.TableEditor;
 import no.ntnu.idatt1002.util.UserUtil;
 
-import java.math.BigDecimal;
 import java.net.URL;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 public final class SettlementEditController extends MenuController implements Initializable {
 
@@ -127,8 +128,6 @@ public final class SettlementEditController extends MenuController implements In
 
     long total = expenses.stream().mapToLong(e -> e.getAmount().longValue()).sum();
     long each = total / settlement.getMembers().size();
-    //System.out.println("total: " + total);
-    //System.out.println("each must pay " + each);
 
     Map<Long, Long> receiveMap = new HashMap<>();
     Map<Long, Long> payMap = new HashMap<>();
@@ -145,15 +144,12 @@ public final class SettlementEditController extends MenuController implements In
     hBox.getChildren().addAll(totalText, totalAmount);
     vBox.getChildren().add(hBox);
     for(long userId : settlement.getMembers()) {
-      //System.out.println("userId " + userId);
       long diff = getUserAmount(userId, expenses) - each;
       Label label = null;
       if(diff < 0) {
-        //System.out.println("user " + userId + " must pay " + Math.abs(diff));
         payMap.put(userId, Math.abs(diff));
         label = new Label(getUser(userId).getUsername() + " must pay " + Math.abs(diff));
       } else if(diff > 0) {
-        //System.out.println("user " + userId + " must receive " + diff);
         receiveMap.put(userId, diff);
         label = new Label(getUser(userId).getUsername() + " must receive " + diff);
       }
@@ -162,85 +158,6 @@ public final class SettlementEditController extends MenuController implements In
       vBox.getChildren().add(label);
     }
     paymentsPane.getChildren().add(vBox);
-  }
-
-  public static void main(String[] args) {
-    List<Long> members = Arrays.asList(1L, 2L, 3L);
-    List<Expense> expenses = Arrays.asList(
-            new Expense(1, 1, new Date(), ExpenseType.CAR,
-                    "Name", BigDecimal.valueOf(10), new Date(), 1),
-            new Expense(2, 2, new Date(), ExpenseType.CAR,
-                    "Name", BigDecimal.valueOf(20), new Date(), 1),
-            new Expense(3, 3, new Date(), ExpenseType.CAR,
-                    "Name", BigDecimal.valueOf(90), new Date(), 1)
-    );
-    long total = expenses.stream().mapToLong(e -> e.getAmount().longValue()).sum();
-    long each = total / members.size();
-    System.out.println("total: " + total);
-    System.out.println("each must pay " + each);
-
-    Map<Long, Long> receiveMap = new HashMap<>();
-    Map<Long, Long> payMap = new HashMap<>();
-
-    for(long userId : members) {
-      long diff = getUserAmount(userId, expenses) - each;
-      if(diff < 0) {
-        System.out.println("user " + userId + " must pay " + Math.abs(diff));
-        payMap.put(userId, Math.abs(diff));
-        //paymentsPane.getChildren().add(new Label());
-      } else if(diff > 0) {
-        System.out.println("user " + userId + " must receive " + diff);
-        receiveMap.put(userId, diff);
-      }
-    }
-
-    Map<Long, Map<Long, Long>> resultMap = new HashMap<>();
-
-    for(long payUserId : payMap.keySet()) {
-      long toPay = payMap.get(payUserId);
-      System.out.println("toPay " + payUserId + " " + toPay);
-      Iterator<Long> receiveIter = receiveMap.keySet().iterator();
-      while(receiveIter.hasNext()) {
-        long receiveUserId = receiveIter.next();
-        long toReceive = receiveMap.get(receiveUserId);
-        if(toPay >= toReceive) {
-          toPay -= toReceive;
-          receiveIter.remove();
-          //Map<Long, Long> m = resultMap.computeIfAbsent(payUserId, k -> new HashMap<>());
-          //m.put(toReceiveId, m.getOrDefault(toReceiveId, 0L) + toReceive);
-        }
-        System.out.println("");
-      }
-    }
-
-        /*for(long payUserId : payMap.keySet()) {
-            long toPay = payMap.get(payUserId);
-            System.out.println("toPay " + payUserId + " " + toPay);
-            Iterator<Map.Entry<Long, Long>> receiveIter = receiveMap.entrySet().iterator();
-            while(receiveIter.hasNext()) {
-                Map.Entry<Long, Long> entry = receiveIter.next();
-                long toReceiveId = entry.getKey();
-                long toReceive = entry.getValue();
-                if(toPay >= toReceive) {
-                    toPay -= toReceive;
-                    receiveIter.remove();
-                    System.out.println("add pay " + toReceiveId + " " + toReceive + " " + toPay);
-                    Map<Long, Long> m = resultMap.computeIfAbsent(payUserId, k -> new HashMap<>());
-                    m.put(toReceiveId, m.getOrDefault(toReceiveId, 0L) + toReceive);
-                } else {
-                    System.out.println("add pay " + toReceiveId + " " + toReceive + " " + toPay);
-                    Map<Long, Long> m = resultMap.computeIfAbsent(payUserId, k -> new HashMap<>());
-                    m.put(toReceiveId, m.getOrDefault(toReceiveId, 0L) + toReceive);
-                }
-            }
-        }*/
-
-    for(long userId : resultMap.keySet()) {
-      for(long targetId : resultMap.get(userId).keySet()) {
-        System.out.println(userId + " must pay " + targetId + " " + resultMap.get(userId).get(targetId));
-      }
-    }
-
   }
 
   private static long getUserAmount(long userId, List<Expense> expenses) {
